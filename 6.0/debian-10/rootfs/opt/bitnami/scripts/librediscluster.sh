dd6a35668d0df8cf4f219fdba8414699a8a33402
LNCrafty <L.reckzulu@gmail.com>
2020-11-18 12:24:11 +0100

    Update librediscluster.sh

--- 6.0/debian-10/rootfs/opt/bitnami/scripts/librediscluster.sh
+++ 6.0/debian-10/rootfs/opt/bitnami/scripts/librediscluster.sh
@@ -89,6 +89,11 @@ redis_cluster_override_conf() {
     if ! (is_boolean_yes "$REDIS_CLUSTER_DYNAMIC_IPS" || is_boolean_yes "$REDIS_CLUSTER_CREATOR"); then
         redis_conf_set cluster-announce-port "$REDIS_CLUSTER_ANNOUNCE_PORT"
     fi
+    
+    if ! (is_boolean_yes "$REDIS_CLUSTER_DYNAMIC_IPS" || is_boolean_yes "$REDIS_CLUSTER_CREATOR"); then
+        redis_conf_set cluster-announce-bus-port "$REDIS_CLUSTER_ANNOUNCE_BUS_PORT"
+    fi
+    
     if is_boolean_yes "$REDIS_TLS_ENABLED"; then
         redis_conf_set tls-cluster yes
         redis_conf_set tls-replication yes
